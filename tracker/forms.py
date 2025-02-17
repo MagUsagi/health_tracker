91843ea71e46bbeec59664de906a32399cd348bf
Akane Klauenboesch <akane.klauenboesch@gmail.com>
2025-02-18 00:31:32 +0100

    Enhance mobile UI: Add navbar toggle, improve graph display, set default date to today

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from .models import HealthRecord
+from datetime import date
 
 class HealthRecordForm(forms.ModelForm):
     class Meta:
         model = HealthRecord
         fields = ['date', 'weight', 'temperature', 'menstruation']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'value': date.today().strftime('%Y-%m-%d')}),
             'weight': forms.NumberInput(attrs={'step': '0.1'}),
             'temperature': forms.NumberInput(attrs={'step': '0.01'}),
         }

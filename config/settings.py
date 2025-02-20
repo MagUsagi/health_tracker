f0e4560bd7a3344a3a06161e0be9b26872bc63fc
Akane Klauenboesch <akane.klauenboesch@gmail.com>
2025-02-20 17:12:45 +0100

    made staticfiles

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = []
-STATIC_ROOT = env('STATIC_ROOT')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
